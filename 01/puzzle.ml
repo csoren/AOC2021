7b212b436fcc02ea9faa53c50d340c1d49899ba3
Carsten Elton Sørensen <cso@rift.dk>
2021-12-02 12:33:30 +0100

    Restructuring

--- 01/puzzle.ml
+++ 01/puzzle.ml
@@ -1,5 +1,4 @@
-module List = ListExtensions
-
+open Extensions
 
 let puzzle_data =
   List.of_textfile_lines "puzzle-input"
@@ -11,32 +10,28 @@ let test_data = [
 ]
 
 
-let int_list_to_string = List.to_string string_of_int
-let int_list_list_to_string = List.to_string int_list_to_string
-
-
-(** [delta list] creates a list of the rate of change between successive elements of [list]  *)
+(** [delta list] creates a list of the change between successive elements of [list]  *)
 let delta list =
   let sub = function
-    | lhs :: rhs :: [] -> rhs - lhs
+    | [lhs; rhs] -> rhs - lhs
     | _ -> 0
   in
-  List.window 2 list |> List.map sub
+  List.window 2 list |> List.map (sub)
 
-
+(** [positive_deltas list] counts the positive changes between successive elements of [list] *)
 let positive_deltas list =
   list |> delta |> List.count (fun v -> v > 0)
 
-
+(** [triplet_sums list] sums the triplets found in a three element sliding window in [list] *)  
+let triplet_sums list =
+  List.window 3 list |> List.map (List.sum (+))
+  
+    
 let puzzle1 () =
   Printf.printf "Test data: %d\n" (test_data |> positive_deltas);
   Printf.printf "Puzzle data: %d\n" (puzzle_data |> positive_deltas)
 
 
-let triplet_sums list =
-  List.window 3 list |> List.map (List.sum (+))
-
-  
 let puzzle2 () =
   Printf.printf "Test data: %d\n" (test_data |> triplet_sums |> positive_deltas);
   Printf.printf "Puzzle data: %d\n" (puzzle_data |> triplet_sums |> positive_deltas)
--- extensions/list.ml
+++ extensions/list.ml
@@ -1,12 +1,15 @@
 include ExtList.List
 
 let count pred list =
-  List.fold_left (fun acc el -> if pred el then acc + 1 else acc) 0 list
+  fold_left (fun acc el -> if pred el then acc + 1 else acc) 0 list
 
 let to_string conv list =
-  let elements = List.map conv list |> String.concat "; " in
+  let elements = map conv list |> String.concat "; " in
   "[" ^ elements ^ "]"
   
+let int_list_to_string = to_string string_of_int
+let int_list_list_to_string = to_string int_list_to_string
+
 let of_textfile_lines file =
   let channel = open_in file in
   let rec loop () =
@@ -19,20 +22,12 @@ let of_textfile_lines file =
     in
   loop ()
 
-let window size list =
-  let rec slide tuple list =
-    match list with
-    | [] -> []
-    | el :: tail ->
-      let n = (drop 1 tuple) @ [el] in
-      n :: slide n tail
-    in
-
+let rec window size list =
   let w = take size list in
   if length w < size then []
-  else w :: slide w (drop size list)
+  else w :: window size (drop 1 list)
 
 let sum op list =
   match list with
   | [] -> failwith "Empty list"
-  | h :: tail -> List.fold_left (fun acc el -> op acc el) h tail
+  | h :: tail -> fold_left op h tail
