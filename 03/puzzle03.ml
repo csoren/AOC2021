63e2dda68f82046ee144e049bda0ac401c9ba299
Carsten Elton Sørensen <cso@rift.dk>
2022-12-06 07:43:24 +0100

    Day 3 progress

--- 03/puzzle03.ml
+++ 03/puzzle03.ml
@@ -5,35 +5,76 @@ open Extensions
 let input =
   File.lines_of "puzzle-input" |> List.of_enum
 
-let is_one = (=) '1'
+let char_is_one = (=) '1'
 
 let bits =
-  input |> List.map (String.explode %> List.map is_one)
+  input |> List.map (String.to_list %> List.map char_is_one) |> Matrix.of_rows
 
-let transposed_bits =  List.transpose bits
-
-let count_ones = List.count_matching Fun.id
+let count_ones =
+  List.count_matching Fun.id
 
 let most_common_bit l =
-  count_ones l > List.length l / 2
+  count_ones l >= List.length l / 2
 
-let most_common_bits list =
-  let cons_bit el list = most_common_bit el :: list in
-  List.fold_right cons_bit list []
+let least_common_bit l =
+  count_ones l < List.length l / 2
 
 let bits_to_int =
   List.fold_left (fun acc bit -> (acc * 2) + Bool.to_int bit) 0
 
-let (gamma, epsilon) =
-  let bits = most_common_bits transposed_bits in
-  (bits |> bits_to_int, bits |> List.map Bool.not |> bits_to_int)
 
+(* Part 1 *)
+
+module Part1 = struct
+  let most_common_bits =
+    Matrix.columns bits |> List.map most_common_bit
+
+  let inverted_bits =
+    most_common_bits |> List.map Bool.not
+
+  let (gamma, epsilon) =
+    (most_common_bits |> bits_to_int, inverted_bits |> bits_to_int)
+
+  let print_solution () =
+    Printf.printf "Part 1, result %d\n" (gamma * epsilon)
+end
+
+
+(* Part 2 *)
+
+module Part2 = struct
+  let most_common_column_bit column m =
+    Matrix.column column m |> most_common_bit
   
-let print_part1 () =
-  print_newline ();
-  Printf.printf "Part 1, result %d\n" (gamma * epsilon)
+  let least_common_column_bit column m =
+    Matrix.column column m |> least_common_bit
 
+  let filter_bit common_bit m column =
+    let bit = common_bit column m in
+    Matrix.filter_rows (fun row -> List.at row column = bit) m
+
+  let filter_values common_bit =
+    List.range 0 `To (Matrix.width bits - 1)
+    |> List.grade (filter_bit common_bit) bits 
+    |> List.find_opt (Matrix.height %> (=) 1)
+    |> Option.get
+
+  let solve common_bit =
+    filter_values common_bit |> Matrix.row 0 |> bits_to_int
+
+  let oxygen = solve most_common_column_bit
+
+  let co2 = solve least_common_column_bit
+
+  let print_solution () =
+    Printf.printf "Oxygen: %d\n" oxygen;
+    Printf.printf "CO2: %d\n" co2;
+    ()
+end
 
 let () =
-  print_part1 ()
-  
+  print_newline ();
+  Part1.print_solution ();
+  Part2.print_solution ()
+
+
