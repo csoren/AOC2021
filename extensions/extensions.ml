26ccd981916bcbd7bd0e7752f89ee613d526a25a
Carsten Elton Sørensen <cso@rift.dk>
2022-12-06 15:24:38 +0100

    Removed extensions

--- extensions/extensions.ml
+++
@@ -1,5 +0,0 @@
-module Array = ExtArray
-module Int = ExtInt
-module List = ExtList
-module Matrix = ExtMatrix
-module String = ExtString
--- extensions/io.ml
+++
@@ -1,3 +0,0 @@
-let line_opt channel = 
-  try Some (input_line channel)
-  with End_of_file -> None
